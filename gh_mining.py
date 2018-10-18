a03f2c4a4c231633fcae573840e95fcad80cd5d9
Arjun B. Gupta <arjun.gupta44@gmail.com>
2018-10-18 11:57:19 -0400

    Adding data

--- gh_mining.py
+++ gh_mining.py
@@ -17,8 +17,8 @@ conn = psycopg2.connect(database="log6307", user = "arjun", password = "", host 
 # Creating a new cursor
 cur = conn.cursor()
 
-# Grabing data from database
-cur.execute("SELECT postid, ghurl FROM goodurls LIMIT 100")
+# Grabing data from database; Randomly picking one of the files referring to one SO post
+cur.execute("SELECT DISTINCT ON (postid) postid, id, ghurl FROM goodurls2 ORDER BY postid, ghurl")
 rows = cur.fetchall()
 
 # Creating an index for progress analysis
@@ -27,22 +27,22 @@ total = len(rows)
 
 # Exctracting the raw file to insert in the table
 for row in rows:
-	print(row[0],row[1])
-	request = requests.get(row[1])
+	request = requests.get(row[2])
 
 	# if file still exist, store contents in the ghurl_contents table in database
 	if request.status_code == 200:
 		data = request.text
 		# Replace new lines with html character so that files fit on one line
 		url_content = data.replace("\n", "&#xa;")
-		cur.execute("INSERT INTO ghurl_contents (postid, data) VALUES (%s, %s)", (row[0], url_content))
+		cur.execute("INSERT INTO ghurl_contents (postid, id, ghurl, data) VALUES (%s, %s, %s, %s)", (row[0], row[1], row[2], url_content))
 
-	# Every 100 files, print the progress
-	if index%100 == 0:
-		print("Progress {:2.1%}".format(100 * index / total), end="\r")
+	# Every 20 files, print the progress
+	if index%20 == 0:
+		print("Progress {:2.2%}".format(index / total), end="\r")
 
 	index += 1
-	
+
+print()
 conn.commit()
 print("Operation done successfully")
 conn.close()
